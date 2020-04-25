247cca53c652e0e5c4fe68017a3bb507c3a2ba84
Laureni Wilkinson <las348@nyu.edu>
2020-04-25 15:25:52 -0400

    update

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ $(document).ready(function () {
 
         alert("note saved");
     });
-      
+
     //get local storage
     $("#8 .col-md-10").val(localStorage.getItem("8"));
     $("#9 .col-md-10").val(localStorage.getItem("9"));
@@ -34,31 +34,41 @@ $(document).ready(function () {
 
 
     //set time 
+    // var amPM = NowMoment.format("A"); //AM or PM
     var hour = NowMoment.format("H");
-    console.log(hour); //24hr
-    var amPM = NowMoment.format("A"); //AM or PM
+    // console.log(hour); //24hr
 
-    for (var i = 8; i < 18; i++) {
-    var daily = moment().startOf('day').add({ hours: [i] }).format('H');
-console.log(daily);
+    am8 = moment().startOf('day').add({ hours: [8] }).format('H A');
+    am9 = (moment().startOf('day').add({ hours: [9] }).format('H A')),
+    am10 = (moment().startOf('day').add({ hours: [10] }).format('H A')),
+    am11 = (moment().startOf('day').add({ hours: [11] }).format('H A')),
+    am12 = (moment().startOf('day').add({ hours: [12] }).format('H A')),
+    am13 = (moment().startOf('day').add({ hours: [13] }).format('H A')),
+    am14 = (moment().startOf('day').add({ hours: [14] }).format('H A')),
+    am15= (moment().startOf('day').add({ hours: [15] }).format('H A')),
+    am16 = (moment().startOf('day').add({ hours: [16] }).format('H A')),
+    am17 = (moment().startOf('day').add({ hours: [17] }).format('H A')),
+    am18 = (moment().startOf('day').add({ hours: [18] }).format('H A')),
+        console.log(am8);
 
-//$("#time8").text(daily); // 5pm
-   // $("#time8").text(daily);
-    // for (var i = 8; i < 18; i++) {
-               
-    // }
-    // //  console.log(dailyJ);
+    $("#time8").text(am8);
+    $("#time9").text(am9);
+    $("#time10").text(am10);
+    $("#time11").text(am11);
+    $("#time12").text(am12);
+    $("#time13").text(am13);
+    $("#time14").text(am14);
+    $("#time15").text(am15);
+    $("#time16").text(am16);
+    $("#time17").text(am17);
+    $("#time18").text(am18);
 
-    //for (var j = 0; j < 9; j++) {
-       // $("#time").text(daily[j]); // M
-     
-
-     }
-
-// updates the past, present, future
+    // updates the past, present, future
     function timeUpdate() {
-        var hour = NowMoment.format("H");
+        //  var hour = NowMoment.format("H");
+        //  var thisHour = parseInt($(this).parent().attr("id"));
         var thisHour = parseInt($(this).parent().attr("id"));
+        //console.log(thisHour);
 
         $(".calendar-row").each(function () {
             if (thisHour < hour) {
